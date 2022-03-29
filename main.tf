4ff9cfeda39abd957e43b60bafdeeaacb4c4fdc8
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-03-28 23:31:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.63"
-    }
-    random = {
-      source = "hashicorp/aws"
-      version = ">=3.0.0"
-    }     
-  }
- 
-  ## Provider us-east-2
+## Provider us-east-2
 provider "aws" {
-  profile = "default"
-  region = "us-east-2"
+    version = "4.8.0"
+    region = "us-east-2"
   
 }
   
