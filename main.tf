98cfa392b7e25c2f47a4bc541115c7749d82b09b
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-03-30 20:32:27 -0400

    heyu

--- main.tf
+++ main.tf
@@ -1,8 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.63"
+    }
+    random = {
+      source = "hashicorp/aws"
+      version = ">=3.0.0"
+    }
+  }
+
+  cloud {
+  organization = "Dataalgebra-Cloud"
+
+    workspaces {
+      name = "AWS-DataalgebraCloud"
+    }
+  }
+}
+
 ## Provider us-east-2
 provider "aws" {
-    version = "4.8.0"
-    region = "us-east-2"
-  
+#  version = "~> 3.0"
+  region = "us-east-2"
+  alias = "oh"
 }
-  
-  
